dce3b1e7ef2dd341352646d28c2221e6bee10282
Maria <92594363+MaryKurinova@users.noreply.github.com>
2021-11-04 18:00:57 +0300

    Create toUpperCase.js
    Одна из самых типовых задачек на собеседовании

---
+++ Введение в JS/Задания/toUpperCase.js
@@ -0,0 +1,11 @@
+Реализуйте и экспортируйте по умолчанию функцию, которая делает заглавной первую букву каждого слова в предложении.
+
+export default (str) => {
+  let result = '';
+  for (let i = 0; i < length(str); i += 1) {
+    const shouldBeBig = str[i] !== ' ' && (i === 0 || str[i - 1] === ' ');
+    result += shouldBeBig ? toUpperCase(str[i]) : str[i];
+  }
+
+  return result;
+};
