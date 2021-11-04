20660d21b570f6595933e11ab2a9dadb6e9346b5
Maria <92594363+MaryKurinova@users.noreply.github.com>
2021-11-04 17:38:36 +0300

    Create SquareofSum.js

---
+++ Введение в JS/Задания/SquareofSum.js
@@ -0,0 +1,13 @@
+Реализуйте функцию squareOfSum()
+
+// @ts-check
+
+// BEGIN (write your solution here)
+const squareOfSum = (a, b) =>
+(a ** 2) + 2 * a * b + (b ** 2);
+
+squareOfSum(2, 3);
+
+// END
+
+export default squareOfSum;
